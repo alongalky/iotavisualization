49befeb391e61448d53c8bb6d9310977ca503167
Alon Gal <alon.gal@iota.org>
2018-01-15 12:22:36 +0200

    Initial commit with sample data

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 9000,
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+
+        // Skip any files outside of your project's `src` directory
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+        ],
+
+        // Only run `.js` and `.jsx` files through Babel
+        test: /\.jsx?$/,
+
+        // Options to configure babel with
+        query: {
+          presets: ['es2015'],
+        },
+      },
+    ],
+  },
+};
